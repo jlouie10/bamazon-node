233794757ac8504d0606c4e1d156d8819c02361d
justin <jlouie10@gmail.com>
2019-06-02 09:21:36 -0500

    Added prompt user for product id and quantity

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -1,4 +1,5 @@
 const mysql = require('mysql');
+const inquirer = require('inquirer');
 
 let connection = mysql.createConnection({
     host: 'localhost',
@@ -19,12 +20,37 @@ let displayProducts = () => {
     connection.query('SELECT * FROM products', (err, rows) => {
         if (err) throw err;
 
-        console.log('ID | Name | Department | Price | Stock')
+        console.log('\nID | Name | Department | Price | Stock')
 
         rows.forEach(element => {
             console.log(`${element.item_id} | ${element.product_name} | ${element.department_name} | $${element.price.toFixed(2)} | ${element.stock_quantity}`);
         });
 
-        connection.end();
+        promptUser();
     });
+};
+
+// Prompts the user for a product selection
+let promptUser = () => {
+    let questions = [
+        {
+            name: 'product',
+            type: 'input',
+            message: "Please enter the ID of the product you wish to purchase."
+        },
+        {
+            name: 'quantity',
+            type: 'input',
+            message: "How many would you like to purchase?"
+        }];
+
+    console.log('_\n');
+
+    inquirer
+        .prompt(questions)
+        .then((answer) => {
+            console.log(`Product ID: ${answer.product}\nQuantity: ${answer.quantity}`);
+
+            connection.end();
+        });
 };

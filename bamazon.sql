63ba3426e35a69cc3c1f3beaee5d36227dbe9655
justin <jlouie10@gmail.com>
2019-06-02 01:30:25 -0500

    Created bamazonCustomer.js, .gitignore, package.json, package-lock.json, bamazon.sql; added display for all items for sale

---
+++ bamazon.sql
@@ -0,0 +1,26 @@
+DROP DATABASE IF EXISTS bamazon;
+
+CREATE DATABASE bamazon;
+
+USE bamazon;
+
+CREATE TABLE products (
+  `item_id` INT NOT NULL AUTO_INCREMENT,
+  `product_name` VARCHAR(255) NOT NULL,
+  `department_name` VARCHAR(255),
+  `price` DECIMAL(10,2) NOT NULL,
+  `stock_quantity` INT NOT NULL,
+  PRIMARY KEY (`item_id`)
+);
+
+INSERT INTO products (`product_name`, `department_name`, `price`, `stock_quantity`)
+VALUES ("Chef's Knife", 'Kitchen', 9.99, 50),
+  ('Cutting Board', 'Kitchen', 5.99, 200),
+  ('Mixing Bowls', 'Kitchen', 22.95, 100),
+  ('Measuring Cups', 'Kitchen', 7.99, 300),
+  ('Can Opener', 'Kitchen', 12.00, 75),
+  ('Colander', 'Kitchen', 13.99, 25),
+  ('Vegetable Peeler', 'Kitchen', 8.75, 50),
+  ('Spoons, Whisks, Spatulas, and Tongs', 'Kitchen', 20.00, 100),
+  ('Thermometer', 'Kitchen', 11.99, 10),
+  ('Potholder', 'Kitchen', 9.99, 125);
---
+++ bamazonCustomer.js
@@ -0,0 +1,32 @@
+const mysql = require('mysql');
+
+let connection = mysql.createConnection({
+    host: 'localhost',
+    port: 8889,
+    user: 'root',
+    password: 'root',
+    database: 'bamazon'
+});
+
+// Connect to the MySQL server
+connection.connect((err) => {
+    if (err) throw err;
+    displayProducts();
+});
+
+// Displays all the products in the database
+let displayProducts = () => {
+    connection.query('SELECT * FROM products', function (err, rows) {
+        if (err) throw err;
+
+        console.log(rows);
+
+        console.log('ID | Name | Department | Price | Stock')
+
+        rows.forEach(element => {
+            console.log(`${element.item_id} | ${element.product_name} | ${element.department_name} | $${element.price.toFixed(2)} | ${element.stock_quantity}`);
+        });
+
+        connection.end();
+    });
+};

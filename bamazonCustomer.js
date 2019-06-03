a733a6202be848e9e18b14c34de8c793d1ea4f5f
justin <jlouie10@gmail.com>
2019-06-02 19:45:38 -0500

    Added database quantity update and print total

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -10,46 +10,97 @@ let connection = mysql.createConnection({
 });
 
 // Connect to the MySQL server
-connection.connect((err) => {
+connection.connect(err => {
     if (err) throw err;
-    displayProducts();
+    // displayProducts();
+    start();
 });
 
-// Displays all the products in the database
-let displayProducts = () => {
+// Query the database for products, display products, and prompt user
+let start = () => {
     connection.query('SELECT * FROM products', (err, rows) => {
         if (err) throw err;
 
-        console.log('\nID | Name | Department | Price | Stock')
+        let products = getProducts(rows);
 
-        rows.forEach(element => {
-            console.log(`${element.item_id} | ${element.product_name} | ${element.department_name} | $${element.price.toFixed(2)} | ${element.stock_quantity}`);
+        displayProducts(products);
+        promptUser(products);
+    });
+};
+
+// Creates an array of products from the database query
+let getProducts = arr => {
+    let productsArr = [];
+
+    arr.forEach(element => {
+        productsArr.push({
+            id: element.item_id,
+            name: element.product_name,
+            department: element.department_name,
+            price: element.price,
+            quantity: element.stock_quantity
         });
+    });
 
-        promptUser();
+    return productsArr;
+};
+
+// Displays all the products in the database
+let displayProducts = arr => {
+    console.log('_\n');
+
+    arr.forEach(element => {
+        console.log(`id: ${element.id}, name: ${element.name}, department: ${element.department}, price: $${element.price.toFixed(2)}, quantity: ${element.quantity}`);
     });
 };
 
 // Prompts the user for a product selection
-let promptUser = () => {
+let promptUser = products => {
     let questions = [
         {
             name: 'product',
             type: 'input',
-            message: "Please enter the ID of the product you wish to purchase."
+            message: 'Please enter the ID of the product you wish to purchase.'
         },
         {
             name: 'quantity',
             type: 'input',
-            message: "How many would you like to purchase?"
+            message: 'How many would you like to purchase?'
         }];
 
     console.log('_\n');
 
     inquirer
         .prompt(questions)
-        .then((answer) => {
-            console.log(`Product ID: ${answer.product}\nQuantity: ${answer.quantity}`);
+        .then(answer => {
+            let productId = parseInt(answer.product);
+            let productQuantity = parseInt(answer.quantity);
+            let productExists = false;
+
+            products.forEach(element => {
+                if (element.id === productId) {
+                    productExists = true;
+                    fulfillOrder(productId, productQuantity, element.price);
+                }
+            });
+
+            if (productExists === false) {
+                console.log('Product ID not found.')
+                connection.end();
+            }
+        });
+};
+
+// Updates quantity in database and prints total
+let fulfillOrder = (id, quantity, price) => {
+    connection.query(
+        `UPDATE products SET stock_quantity = stock_quantity - ${quantity} WHERE item_id = ${id}`,
+        err => {
+            if (err) throw err;
+
+            let total = quantity * price;
+
+            console.log(`The total cost of your purchase is $${total.toFixed(2)}`);
 
             connection.end();
         });

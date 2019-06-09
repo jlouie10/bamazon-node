a2099be84226438819ba9772638d2b14b56732c1
justin <jlouie10@gmail.com>
2019-06-09 12:15:59 -0500

    Updated schema.sql added products for Books department

--- schema.sql
+++ schema.sql
@@ -20,10 +20,10 @@ VALUES ("Chef's Knife", 'Kitchen', 9.99, 50),
   ('Measuring Cups', 'Kitchen', 7.99, 300),
   ('Can Opener', 'Kitchen', 12.00, 75),
   ('Colander', 'Kitchen', 13.99, 25),
-  ('Vegetable Peeler', 'Kitchen', 8.75, 50),
-  ('Spoons, Whisks, Spatulas, and Tongs', 'Kitchen', 20.00, 100),
-  ('Thermometer', 'Kitchen', 11.99, 10),
-  ('Potholder', 'Kitchen', 9.99, 125);
+  ("Gordon Ramsay's Ultimate Cookery", 'Books', 70.00, 50),
+  ('The Martha Stewart Cookbook', 'Books', 24.99, 100),
+  ('Alinea by Grant Achatz', 'Books', 99.99, 10),
+  ('Kitchen Confidential', 'Books', 14.99, 125);
 
 CREATE TABLE departments (
   `department_id` INT NOT NULL AUTO_INCREMENT,
